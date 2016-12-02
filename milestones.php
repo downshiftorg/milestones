1bf4cc67102bc0e46f9a27343133247d5fe33f21
Brian Scaturro <scaturrob@gmail.com>
2016-12-02 11:27:48 -0500

    Adds milestone constant

--- milestones.php
+++ milestones.php
@@ -11,6 +11,7 @@ Author: Brian Scaturro
 Version: 1.0.0
 Author URI: http://github.com/brianium
 */
+define('MILESTONES_ENABLED', true);
 define('MILESTONES_VIEW_DIR', __DIR__ . '/views');
 require  __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/plugin.php';
